de2caf8e0849be49315a1825a91782f319ffa341
Jake Richan <jake.richan@gmail.com>
2019-09-09 11:25:05 -0600

    add eslint-plugin-import to auto sort import statements

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 module.exports = {
-    extends: [
-        'eslint:recommended',
-        'standard',
-        'standard-react',
-        'plugin:react/recommended',
-        'plugin:jsx-a11y/recommended',
-        'plugin:jest/recommended',
-    ],
-    parser: 'babel-eslint',
-    plugins: [
-        'jsx-a11y',
-        'jest',
-        'emotion',
-        'react-hooks'
-    ],
-    rules: {
-        'no-console': [1, { allow: ['warn', 'error'] }],
-        'lines-between-class-members': 1,
-        "react/jsx-sort-props": ["error"],
-        "react/jsx-wrap-multilines": ["error", {
-            "declaration": "parens-new-line",
-            "assignment": "parens-new-line",
-            "return": "parens-new-line",
-            "arrow": "parens-new-line",
-            "condition": "parens-new-line",
-            "logical": "parens-new-line",
-            "prop": "parens-new-line"
-        }],
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn'
-    }
+  extends: [
+    'eslint:recommended',
+    'standard',
+    'standard-react',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:jest/recommended',
+  ],
+  parser: 'babel-eslint',
+  plugins: [
+    'jsx-a11y',
+    'jest',
+    'emotion',
+    'react-hooks',
+    'import'
+  ],
+  rules: {
+    'no-console': [1, { allow: ['warn', 'error'] }],
+    'lines-between-class-members': 1,
+    "react/jsx-sort-props": ["error"],
+    "react/jsx-wrap-multilines": ["error", {
+      "declaration": "parens-new-line",
+      "assignment": "parens-new-line",
+      "return": "parens-new-line",
+      "arrow": "parens-new-line",
+      "condition": "parens-new-line",
+      "logical": "parens-new-line",
+      "prop": "parens-new-line"
+    }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    "import/order": ["error", {
+      "groups": ["builtin", "external", "unknown", "internal", "parent", "sibling", "index"]
+    }]
+  }
 }
